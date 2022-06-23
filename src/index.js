20af0eb53762641ac28a9ac5f0fd22f4218fa5ca
alexeyShuncko <shuncko.a@yandex.ru>
2022-06-23 09:03:57 +0300

    Удаление

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import reportWebVitals from './reportWebVitals';
+
 import store from './Redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -18,8 +18,3 @@ import { Provider } from 'react-redux';
     </React.StrictMode>,
     document.getElementById('root'));
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
