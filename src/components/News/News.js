835a7429b4ec6febefc3ab6a45bf81128d611700
alexeyShuncko <shuncko.a@yandex.ru>
2022-07-13 18:05:09 +0300

    Новости макет

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,15 +1,108 @@
-import { Container } from '@mui/material';
+import { Box, Button, Card, CardContent, CardMedia, Container, Typography } from '@mui/material';
 import React from 'react';
 // import s from './News.module.css';
+import foto1 from '../../image/news/foto.jpg'
 
 
 
 const News = (props) => {
 
+
+    const arrNews = [
+        {
+            name: '', date: '', photo: '', text: '', id: 0
+        }
+    ]
+
     return (
-       <Container maxWidth={'md'} sx={{ mt: '6rem' }}>
-        News
-       </Container>
+        <Container maxWidth={'md'} sx={{ mt: '6rem', display: 'flex' }} >
+            <Box sx={{ flexGrow: 2 }}>
+
+                <Card sx={{ maxWidth: 500, mb: 2 }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            News 1
+                        </Typography>
+                        <Typography variant="subtitle2" component="div">
+                            22-02-2022 16:27
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        component="img"
+                        height="250"
+                        image={foto1}
+                        alt="news1"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Button variant="contained" color='success'>Add news</Button>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}>
+
+                <Card sx={{ maxWidth: 300, display: 'flex', cursor: 'pointer', mb: 1}}>
+                    <CardContent sx={{ flexGrow: 2 }}>
+                        <Typography variant="body2" >
+                            News awdawdawdawdawda wdawdawdawdawd awdawdawdawdawdawdawd
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        sx={{ flexGrow: 1, py: 2 }}
+                        component="img"
+                        height="80"
+                        image={foto1}
+                        alt="news1"
+                    />
+                </Card>
+                <Card sx={{ maxWidth: 300, display: 'flex', cursor: 'pointer', mb: 1 }}>
+                    <CardContent sx={{ flexGrow: 2 }}>
+                        <Typography variant="body2" >
+                            News awdawdawdawdawda wdawdawdawdawd awdawdawdawdawdawdawd
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        sx={{ flexGrow: 1, py: 2}}
+                        component="img"
+                        height="80"
+                        image={foto1}
+                        alt="news1"
+                    />
+                </Card>
+                   <Card sx={{ maxWidth: 300, display: 'flex', cursor: 'pointer', mb: 1 }}>
+                    <CardContent sx={{ flexGrow: 2 }}>
+                        <Typography variant="body2" >
+                            News awdawdawdawdawda wdawdawdawdawd awdawdawdawdawdawdawd
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        sx={{ flexGrow: 1, py: 2 }}
+                        component="img"
+                        height="80"
+                        image={foto1}
+                        alt="news1"
+                    />
+                </Card>
+                <Card sx={{ maxWidth: 300, display: 'flex', cursor: 'pointer', mb: 1 }}>
+                    <CardContent sx={{ flexGrow: 2 }}>
+                        <Typography variant="body2" >
+                            News awdawdawdawdawda wdawdawdawdawd awdawdawdawdawdawdawd
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        sx={{ flexGrow: 1, py: 2 }}
+                        component="img"
+                        height="80"
+                        image={foto1}
+                        alt="news1"
+                    />
+                </Card>
+                <Button variant='outlined'>Show all news</Button>
+            </Box>
+        </Container>
     )
 }
 
